0598d1f34865c20ceffec1ea4f5558a734c1a071
Sheen Patel <programmingpatel@gmail.com>
2021-06-28 15:46:36 -0400

    added plug installer because why have I done this manually all these years

--- install.sh
+++ install.sh
@@ -5,12 +5,14 @@
 if [[ "$OSTYPE" == "linux"* ]]; then
         # install with linux apt-get
 		sudo apt-get install neovim
+		sudo apt-get install curl
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         # install with brew
 		brew install neovim
 else
 	echo "you will need to install neovim and things manually because I have not implemented a way to skip this if neovim installed lmao. TODO! college admissions person or whoever looks at this please remind me if not done. /shrug"
 	sudo apt-get install neovim
+	sudo apt-get install curl
 fi
 
 #copy the vimrc to the right directory
@@ -20,6 +22,9 @@ cp ./init.vim ~/.config/nvim/init.vim
 #install the python thing
 sudo apt-get install python3-pip
 pip3 install neovim
+
+# do the plug thing
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 #open and close neovim to install vim-plug and extensions.
 nvim -c ":qa"
